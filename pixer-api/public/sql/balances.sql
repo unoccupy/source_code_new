d13e206014cbd408c4c8e95db1fd37dcdd5cb740
harshithva <harshith11032001@gmail.com>
2022-10-24 10:18:19 +0530

    initial

---
+++ pixer-api/public/sql/balances.sql
@@ -0,0 +1,10 @@
+INSERT INTO `balances` (`id`, `shop_id`, `admin_commission_rate`, `total_earnings`, `withdrawn_amount`, `current_balance`, `payment_info`, `created_at`, `updated_at`) VALUES
+(1, 1, 10, 310.5, 124.2, 186.3, '{\"bank\": null, \"name\": null, \"email\": \"admin@example.com\"}', '2022-01-22 16:30:50', '2022-02-24 04:56:16'),
+(2, 2, 10, 79.2, 0, 79.2, '{\"bank\": null, \"name\": null, \"email\": \"rnb@rnc.rnd\"}', '2022-01-22 16:51:34', '2022-02-20 06:24:39'),
+(3, 3, 10, 186.273, 0, 186.273, '{\"bank\": null, \"name\": null, \"email\": \"furniture_shop@demo.com\"}', '2022-01-22 16:53:14', '2022-08-11 06:52:09'),
+(4, 4, 10, 1286.1, 0, 1286.1, '{\"bank\": null, \"name\": null, \"email\": \"admin@example.com\"}', '2022-01-26 08:55:45', '2022-05-11 08:10:25'),
+(5, 5, 10, 549.9, 0, 549.9, '{\"bank\": null, \"name\": null, \"email\": \"shop_owner@demo.com\"}', '2022-01-26 09:00:06', '2022-05-11 08:10:25'),
+(6, 6, 10, 1033.2, 0, 1033.2, '{\"bank\": null, \"name\": null, \"email\": \"admin@example.com\"}', '2022-01-26 15:51:40', '2022-05-11 08:10:25'),
+(7, 7, 15, 453.9, 0, 453.9, '{\"bank\": null, \"name\": null, \"email\": \"admin@example.com\"}', '2022-01-26 15:53:03', '2022-05-11 08:10:24'),
+(8, 8, 20, 1396, 0, 1396, '{\"bank\": null, \"name\": null, \"email\": \"rnb@rnc.rnd\"}', '2022-01-26 15:54:49', '2022-05-11 08:10:25'),
+(9, 9, 10, 856.8, 435, 421.8, '{\"bank\": \"demo\", \"name\": \"demo\", \"email\": \"shop_owner@demo.com\", \"account\": 1231321321}', '2022-01-26 15:56:04', '2022-05-11 08:10:25');
---
+++ pixer-api/public/sql/order_status.sql
@@ -0,0 +1,7 @@
+INSERT INTO `order_status` (`id`, `name`, `slug`, `serial`, `language`, `color`, `created_at`, `updated_at`) VALUES
+(1, 'Confirmed', 'confirmed', 1, 'en', '#0d9965', '2022-02-02 21:08:20', '2022-02-22 03:46:30'),
+(2, 'On hold', 'on-hold', 2, 'en', '#64acd8', '2022-02-19 16:37:55', '2022-02-19 16:37:55'),
+(3, 'Processing', 'processing', 3, 'en', '#eacb34', '2022-02-23 14:55:01', '2022-02-23 14:55:01'),
+(4, 'wird bearbeitet', 'processing', 3, 'de', '#eacb34', '2022-08-09 07:04:18', '2022-08-09 07:04:18'),
+(5, 'In Wartestellung', 'on-hold', 2, 'de', '#64acd8', '2022-08-09 07:04:37', '2022-08-09 07:04:37'),
+(6, 'Bestätigt', 'confirmed', 1, 'de', '#0d9965', '2022-08-09 07:04:58', '2022-08-09 07:04:58');
